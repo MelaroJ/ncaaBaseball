026d0e872480feecb612f2b40c126f8a02c04b44
MelaroJ <melarojake@gmail.com>
2025-06-07 16:38:11 -0400

    updated description with glue, lubridate, rlang, and tibble packages.
    added school_codes.R for description of internal data.

---
+++ R/data_school_codes.R
@@ -0,0 +1,11 @@
+#' NCAA School Codes
+#'
+#' A cached dataset of `org_id` and `school_name` scraped from stats.ncaa.org.
+#'
+#' @format A tibble with columns:
+#' \describe{
+#'   \item{org_id}{integer, unique team ID}
+#'   \item{school_name}{character, institution name}
+#' }
+#' @source \url{https://stats.ncaa.org/teams/history}
+"school_codes"
